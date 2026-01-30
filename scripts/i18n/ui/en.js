29b39eac915c606e4bde088ea8a3c93663c74fcc
Diarapak <krobpierre0@gmail.com>
2026-01-30 12:33:52 +0100

    add trad menu

--- scripts/i18n/ui/en.js
+++ scripts/i18n/ui/en.js
@@ -16,30 +16,41 @@ i18n.register('ui', 'en', {
     wipeDataConfirm: "Are you sure you want to delete all data forever?",
     yeah: "Yeah!",
     language: "Language",
-    
+
     // Menu items
+    travel: "Travel",
+    vs: "VS",
+    items: "Items",
     teams: "Teams",
+    dex: "Dex",
+    shop: "Poke-Mart",
+    training: "Training",
+    genetics: "Genetics",
+    mysteryGift: "Mystery Gift",
+    exportReward: "Export Reward",
+    wonderTrade: "Wonder Trade",
     dictionary: "Dictionary",
-    explore: "Explore",
-    shop: "Shop",
-    
+    guide: "Guide",
+    settings: "Settings",
+    fullScreen: "Full Screen",
+
     // Item menu
     held: "Held",
     tms: "TM's",
     key: "Key",
     cancel: "Cancel",
     remove: "Remove",
-    
+
     // Combat
     leaveCombat: "Leave Combat",
-    
+
     // Credits
     creditsText: "Made by Duck<br><br>Pokemon and trainer sprites by Pokemon Showdown (Including artists: Beliot419, kyledove, Gnomowladny, Brumirage and ZacWeavile. Go check their work. Now.)<br><br>Gen 9 sprites by KingOfThe-X-Roads<br><br>All characters and related assets featured in this game are the exclusive property of Nintendo, Creatures Inc., and The Pokémon Company, and are used here strictly for non-commercial purposes.",
-    
+
     // New items notification
     newItems: "New items!",
     vsRewardsUpdate: "Due to the vs rewards update you have been rewarded for your defeated trainers:",
-    
+
     // Disclaimer
     disclaimerText: "This project is entirely unofficial!<br><br>All characters and related intellectual property featured are the exclusive property of Nintendo, Creatures Inc., and The Pokémon Company.",
 });
--- scripts/i18n/ui/fr.js
+++ scripts/i18n/ui/fr.js
@@ -16,30 +16,41 @@ i18n.register('ui', 'fr', {
     wipeDataConfirm: "Êtes-vous sûr de vouloir supprimer toutes les données définitivement ?",
     yeah: "Ouais !",
     language: "Langue",
-    
+
     // Éléments du menu
-    teams: "Équipes",
+    travel: "Voyage",
+    vs: "VS",
+    items: "Objets",
+    team: "Équipes",
+    dex: "Dex",
+    shop: "Poké-Shop",
+    training: "Entraînement",
+    genetics: "Génétique",
+    mysteryGift: "Cadeau Mystère",
+    exportReward: "Récompense d'Exportation",
+    wonderTrade: "Échange Mystère",
     dictionary: "Dictionnaire",
-    explore: "Explorer",
-    shop: "Boutique",
-    
+    guide: "Guide",
+    settings: "Paramètres",
+    fullScreen: "Plein écran",
+
     // Menu des objets
     held: "Tenus",
     tms: "CT",
     key: "Clés",
     cancel: "Annuler",
     remove: "Retirer",
-    
+
     // Combat
     leaveCombat: "Quitter le combat",
-    
+
     // Crédits
     creditsText: "Créé par Duck<br><br>Sprites Pokémon et dresseurs par Pokemon Showdown (y compris les artistes : Beliot419, kyledove, Gnomowladny, Brumirage et ZacWeavile. Allez voir leur travail. Maintenant.)<br><br>Sprites Gen 9 par KingOfThe-X-Roads<br><br>Tous les personnages et éléments associés présents dans ce jeu sont la propriété exclusive de Nintendo, Creatures Inc. et The Pokémon Company, et sont utilisés ici strictement à des fins non commerciales.",
-    
+
     // Notification nouveaux objets
     newItems: "Nouveaux objets !",
     vsRewardsUpdate: "Suite à la mise à jour des récompenses VS, vous avez été récompensé pour vos dresseurs vaincus :",
-    
+
     // Avertissement
     disclaimerText: "Ce projet est entièrement non officiel !<br><br>Tous les personnages et propriétés intellectuelles présentés sont la propriété exclusive de Nintendo, Creatures Inc. et The Pokémon Company.",
 });
